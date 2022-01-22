3e78c1a2a65f08baca672af0e458d0ec77e7b675
Max3526 <47460467+Max3526@users.noreply.github.com>
2022-01-22 01:47:47 +0100

    Update conversion50.5.js

--- Commandes/conversion50.5.js
+++ Commandes/conversion50.5.js
@@ -1,7 +1,7 @@
 module.exports.run = (client, message, args) => {
     let début = Date.now();
     message.channel.send('size50.5')
-        .then((m) => m.edit(`La taille : **50.5 EU** correspond à :  UK M --> **15** / US *--> **16** / US W --> **10**`));
+        .then((m) => m.edit(`La taille : **50.5 EU** correspond à :  UK M --> **15** / US --> **16** / US W --> **10**`));
 };
 
 module.exports.help = {
